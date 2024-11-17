0d9cb90d281b1f3daa1c8a4fb9dd8f42051f6b0b
Luna <hamanhduy1805@gmail.com>
2024-11-17 17:22:09 +0700

    .

--- index.js
+++ index.js
@@ -43,6 +43,48 @@ app.get('/api/kho', async (req, res) => {
   }
 });
 
+app.get('/api/khoBTP', async (req, res) => {
+  const TenNha = req.query.TenNha;
+  const ID_Kho = parseInt(req.query.ID_Kho);
+  const MaVung = req.query.MaVung;
+  try {
+    let pool = await sql.connect(config);
+
+    // Gọi stored procedure với 3 tham số
+    let result = await pool.request()
+      .input('TenNha', sql.VarChar, TenNha)
+      .input('Id_kho', sql.Int, ID_Kho)
+      .input('MaVung', sql.VarChar, MaVung)
+      .execute('BaoCao_Ton_Layoutkho_BTP'); // Tên stored procedure của bạn
+
+    // Trả về dữ liệu JSON cho frontend
+    res.json(result.recordset);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Có lỗi xảy ra khi kết nối với cơ sở dữ liệu.');
+  }
+});
+
+app.get('/api/khoBTP/search', async (req, res) => {
+  const So_LenhXUATVT = req.query.So_LenhXUATVT;
+  const ID_Kho = parseInt(req.query.ID_Kho);
+  const MaVung = req.query.MaVung;
+  try {
+    let pool = await sql.connect(config);
+
+    // Gọi stored procedure với 3 tham số
+    let result = await pool.request()
+      .input('So_LenhXUATVT', sql.VarChar, 'LXVT-2024-10-0288')
+      .input('Ma_Vattu', sql.VarChar, '')
+      .execute('BaoCao_ViTriVatTu'); // Tên stored procedure của bạn
+
+    // Trả về dữ liệu JSON cho frontend
+    res.json(result.recordset);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Có lỗi xảy ra khi kết nối với cơ sở dữ liệu.');
+  }
+});
 app.get('/api/kho2', async (req, res) => {
   // const iD_Kho = parseInt(req.query.iD_Kho);
   // const tenNha = req.query.tenNha;
@@ -55,7 +97,7 @@ app.get('/api/kho2', async (req, res) => {
     let result = await pool.request()
       .input('iD_Kho', sql.Int, 1)
       .input('tenNha', sql.VarChar, "Kho N1")
-      .input('Mavitrikho', sql.VarChar, "A311")
+      .input('Mavitrikho', sql.VarChar, "A111")
       .input('MaVung', sql.VarChar, 'A')
       .execute('BaoCao_TonViTri'); // Tên stored procedure của bạn
 
